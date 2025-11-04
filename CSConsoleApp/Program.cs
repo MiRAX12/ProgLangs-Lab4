5fa5c7bedb6038748c158d3dbb4af2e3feec83fe
MiRAX12 <nagorniy.kolya2013@gmail.com>
2025-11-04 19:53:36 +0300

    Добавьте файлы проекта.

---
+++ CSConsoleApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CSConsoleApp
+{
+    public static class Program
+    {
+
+        public static void Main()
+        {
+            ResultPrinter resultPrinter = new ResultPrinter();
+
+            PrintResult("Все фильмы, снятые Стивеном Спилбергом:", resultPrinter.GetSpielbergMovies());
+            PrintResult("Персонажи, которых сыграл актер Tom Hanks.", resultPrinter.GetTomHanksCharacters());
+            PrintResult("5 фильмов с самым большим количеством актеров", resultPrinter.GetMostEnormousCast());
+            PrintResult("Топ-10 самых востребованных актеров", resultPrinter.GetTopTenActors());
+            PrintResult("Список всех уникальных департаментов", resultPrinter.getAllDepartments());
+            PrintResult("Все фильмы, где Hans Zimmer был композитором", resultPrinter.getZimmerComposer());
+            PrintDictionary("Создать словарь, где ключ — ID фильма, а значение — имя режиссера.", resultPrinter.getDictionary());
+            PrintResult("Найти фильмы, где в актерском составе есть и \"Brad Pitt\", и \"George Clooney\".", resultPrinter.getBradAndClooney());
+            resultPrinter.getCameraDepartmentCount();
+            PrintResult("Найти всех людей, которые в фильме \"Titanic\" были одновременно и в съемочной группе, и в списке актеров.",
+                resultPrinter.getActorCrew());
+            PrintResult("Найти \"внутренний круг\" режиссера: Для режиссера \"Quentin Tarantino\" найти топ-5 членов съемочной группы (не актеров)," +
+                " которые работали с ним над наибольшим количеством фильмов.\r\n",
+                resultPrinter.getTarantinoFiveCrew());
+            PrintResult("Определить экранные \"дуэты\": Найти 10 пар актеров, которые чаще всего снимались вместе в одних и тех же фильмах.",
+                resultPrinter.getPopularPairs());
+            PrintResult("Вычислить \"индекс разнообразия\" для карьеры: Найти 5 членов съемочной группы, " +
+                "которые поработали в наибольшем количестве различных департаментов за свою карьеру.", resultPrinter.getIndex());
+            PrintResult("Найти \"творческие трио\": Найти фильмы, где один и тот же человек выполнял роли режиссера" +
+                " (Director), сценариста (Writer) и продюсера (Producer).", resultPrinter.getTrio());
+            PrintResult("Два шага до Кевина Бейкона: Найти всех актеров, которые снимались в одном фильме с актером," +
+                " который, в свою очередь, снимался в одном фильме с \"Kevin Bacon\".", resultPrinter.getKevinTwoSteps());
+            PrintResult("Проанализировать \"командную работу\": Сгруппировать фильмы по режиссеру и для каждого" +
+                " из них найти средний размер как актерского состава (Cast), так и съемочной группы (Crew).", resultPrinter.getTeamWork());
+            PrintResult("Найти пересечение \"элитных клубов\": Найти людей, которые работали и" +
+                " с режиссером \"Martin Scorsese\", и с режиссером \"Christopher Nolan\".", resultPrinter.getMartinAndNolan());
+            PrintResult("Выявить \"скрытое влияние\": Ранжировать все департаменты по" +
+                " среднему количеству актеров в тех фильмах, над которыми они работали" +
+                " (чтобы проверить, коррелирует ли работа определенного департамента с масштабом актерского состава).",
+                resultPrinter.getHiddenImpact());
+            PrintResult("Проанализировать \"архетипы\" персонажей:" +
+                " Для актера \"Johnny Depp\" сгруппировать его роли по первому слову" +
+                " в имени персонажа (например, \"Captain\", \"Jack\", \"Willy\")" +
+                " и посчитать частоту каждого такого \"архетипа\".", resultPrinter.getJohnnyArchetypes());
+        }
+
+        private static void PrintResult(string title, IEnumerable<string> result)
+        {
+            Console.Clear();
+            Console.WriteLine(title);
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+            Console.WriteLine("\nНажмите любую клавишу чтобы продолжить");
+            Console.ReadKey();
+        }
+
+        private static void PrintDictionary(string title, Dictionary<int, string> dictionary)
+        {
+            Console.WriteLine("Словарь на " + dictionary.Count + " элементов создан. Желаете просмотреть его?\ny/n");
+            var key = Console.ReadKey(false).Key;
+            if (key == ConsoleKey.Y) {
+                foreach (var director in dictionary)
+                {
+                    Console.WriteLine(director.Key + "Режисер: " + director.Value);
+                }
+            }
+            Console.WriteLine("\nНажмите любую клавишу чтобы продолжить");
+            Console.ReadKey();
+            return;
+            
+        }
+    }
+}
---
+++ CSConsoleApp/ResultPrinter.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Sources;
+
+namespace CSConsoleApp
+{
+    internal class ResultPrinter
+    {
+        private string currentDirectory;
+        private string filePath;
+        private IReadOnlyList<MovieCredit> movieCredits;
+
+        public ResultPrinter()
+        {
+            currentDirectory = System.IO.Directory.GetCurrentDirectory();
+            filePath = System.IO.Directory.GetFiles(currentDirectory, "*.csv").First();
+            try
+            {
+                movieCredits = new MovieCreditsParser(filePath).Parse();
+            } catch (Exception ex) {
+                Console.WriteLine("Не удалось распарсить csv");
+                Environment.Exit(1);
+            }
+        }
+
+        public IEnumerable<string> GetSpielbergMovies()
+        {
+            var StevenSpielbergMovies = from movie in movieCredits
+                                        from CrewMember in movie.Crew
+                                        where CrewMember.Name.Equals("Steven Spielberg")
+                                        select movie;
+            return StevenSpielbergMovies.Distinct().Select(m => m.Title);
+        }
+
+        public IEnumerable<string> GetTomHanksCharacters()
+        {
+            var TomHanksCharacters = from movie in movieCredits
+                                     from CastMember in movie.Cast
+                                     where CastMember.Name.Equals("Tom Hanks")
+                                     select CastMember.Character;
+            return TomHanksCharacters;
+        }
+
+        public IEnumerable<string> GetMostEnormousCast()
+        {
+            var TopFiveCast = (from movie in movieCredits
+                              orderby movie.Cast.Count descending
+                              select movie)
+                              .Take(5);
+            return TopFiveCast.Select(m => $"{m.Title} - {m.Cast.Count}");
+        }
+
+        public IEnumerable<string> GetTopTenActors()
+        {
+            var top10Actors = movieCredits
+                                .SelectMany(movie => movie.Cast) 
+                                .GroupBy(castMember => castMember.Name) 
+                                .Select(group => new
+                                {
+                                    ActorName = group.Key,
+                                    MovieCount = group.Count()
+                                })
+                                .OrderByDescending(actor => actor.MovieCount) 
+                                .Take(10);
+            return top10Actors.Select(a => $"{a.ActorName} - {a.MovieCount}");
+
+        }
+
+        public IEnumerable<string> getAllDepartments()
+        {
+            var AllDepartmants = from movie in movieCredits
+                                  from CrewMember in movie.Crew
+                                  select CrewMember.Department;
+            return AllDepartmants.Distinct();
+        }
+
+        public IEnumerable<string> getZimmerComposer()
+        {
+            var AllMovies = from movie in movieCredits
+                            from CrewMember in movie.Crew
+                            where CrewMember.Job.Equals("Original Music Composer")
+                            where CrewMember.Name.Equals("Hans Zimmer")
+                            select movie;
+            return AllMovies.Select(m => m.Title);
+        }
+
+        public Dictionary<int, string> getDictionary()
+        {
+            var Directors = movieCredits.ToDictionary(movie => movie.MovieId,
+                    movie => movie.Crew.FirstOrDefault(c => c.Job.Equals("Director"))
+                        ?.Name ?? "Неизвестен"
+                );
+            return Directors;
+        }
+
+        public IEnumerable<string> getBradAndClooney()
+        {
+            var AllMovies = from movie in movieCredits
+                            let actorNames = movie.Cast.Select(c => c.Name).ToList()
+                            where actorNames.Contains("Brad Pitt") && actorNames.Contains("George Clooney")
+                            select movie;
+            return AllMovies.Select(m => m.Title);
+        }
+
+        public int getCameraDepartmentCount()
+        {
+            var CameraDepartmentCount = (from movie in movieCredits
+             from crewMember in movie.Crew
+             where crewMember.Department.Equals("Camera")
+             select crewMember.Id)
+                .Distinct()
+                .Count();
+            return CameraDepartmentCount;
+        }
+
+        public IEnumerable<string> getActorCrew()
+        {
+            var titanicMembers =
+                (from movie in movieCredits
+                 where movie.Title.Equals("Titanic")
+                 from actor in movie.Cast
+                 from crew in movie.Crew
+                 where actor.Name.Equals(crew.Name)
+                 select actor.Name)
+                .Distinct();
+            return titanicMembers;
+        }
+
+        public IEnumerable<string> getTarantinoFiveCrew()
+        {
+            var tarantinoCrew =
+                (from movie in movieCredits
+                 where movie.Crew.Any(c => c.Job.Equals("Director")
+                 && c.Name.Equals("Quentin Tarantino"))
+                 from CrewMember in movie.Crew
+                 where !CrewMember.Name.Equals("Quentin Tarantino")
+                 group CrewMember by CrewMember.Name into CrewGroup
+                 orderby CrewGroup.Count() descending
+                 select new
+                 {
+                     name = CrewGroup.Key,
+                     movieCount = CrewGroup.Count()
+                 }).Take(5);
+            return tarantinoCrew.Select(m => $"{m.name} - {m.movieCount}").Distinct();
+        }
+
+        public IEnumerable<string> getPopularPairs()
+        {
+            var popularPairs =
+                (from movie in movieCredits
+                 from a1 in movie.Cast
+                 from a2 in movie.Cast
+                 where string.Compare(a1.Name, a2.Name) < 0
+                 group movie by new { actor1 = a1.Name, actor2 = a2.Name } into pairGroup
+                 orderby pairGroup.Count() descending
+                 select new
+                 {
+                     Pair = $"{pairGroup.Key.actor1} & {pairGroup.Key.actor2}",
+                     MoviesCount = pairGroup.Count()
+                 }).Take(5);
+            return popularPairs.Select(m => $"{m.Pair} - {m.MoviesCount}");
+        }
+
+        public IEnumerable<string> getIndex()
+        {
+            var index =
+                (from movie in movieCredits
+                 from crewMember in movie.Crew
+                 group crewMember by crewMember.Name into crewGroup
+                 let departmentCount = crewGroup
+                     .Select(c => c.Department)
+                     .Distinct()
+                     .Count()
+                 orderby departmentCount descending
+                 select new
+                 {
+                     Name = crewGroup.Key,
+                     DepartmentCount = departmentCount
+                 })
+                .Take(5);
+            return index.Select(m => $"{m.Name} - {m.DepartmentCount}");
+        }
+
+        public IEnumerable<string> getTrio()
+        {
+            var trio =
+                (from movie in movieCredits
+                from crewMember in movie.Crew
+                let personJobs = movie.Crew
+                    .Where(c => c.Name == crewMember.Name)
+                    .Select(c => c.Job)
+                    .ToList()
+                where personJobs.Contains("Director")
+                   && personJobs.Contains("Writer")
+                   && personJobs.Contains("Producer")
+                select new
+                {
+                    Movie = movie.Title,
+                    Person = crewMember.Name
+                }).Distinct();
+            return trio.Select(m => $"{m.Movie} - {m.Person}");
+        }
+
+        public IEnumerable<string> getKevinTwoSteps()
+        {
+            var oneStep =
+                (from movie in movieCredits
+                 where movie.Cast.Any(a => a.Name.Equals("Kevin Bacon"))
+                 from actor in movie.Cast
+                 where !actor.Name.Equals("Kevin Bacon")
+                 select actor.Name)
+                .Distinct().ToList();
+
+            var twoSteps =
+                (from movie in movieCredits
+                 from actor in movie.Cast
+                 where movie.Cast.Any(a => oneStep.Contains(a.Name))
+                 where !actor.Name.Equals("Kevin Bacon")
+                 select actor)
+                .Distinct();
+            return twoSteps.Select(a => a.Name);
+        }
+        
+        public IEnumerable<string> getTeamWork()
+        {
+            var teamWork =
+                from movie in movieCredits
+                from director in movie.Crew
+                where director.Job.Equals("Director", StringComparison.OrdinalIgnoreCase)
+                group movie by director.Name into directorGroup
+                select new
+                {
+                    Director = directorGroup.Key,
+                    AverageCastSize = directorGroup.Average(m => m.Cast.Count),
+                    AverageCrewSize = directorGroup.Average(m => m.Crew.Count)
+                };
+            return teamWork.Select(m => $"Director: {m.Director}   -   AverageCastSize: {m.AverageCastSize}, " +
+            $"AverageCrewSize: {m.AverageCrewSize}");
+        }
+
+        public IEnumerable<string> getUniversals()
+        {
+            var actorNames = (from movie in movieCredits
+                              from cast in movie.Cast
+                              select cast.Name)
+                 .Distinct()
+                 .ToList();
+            var crewNames = (from movie in movieCredits
+                             from crew in movie.Crew
+                             select crew.Name)
+                            .Distinct()
+                            .ToList();
+            var universals = actorNames.Intersect(crewNames).ToList();
+
+            var universalsTopDepartments =
+                from nameUniversal in universals
+                let departments =
+                    (from movie in movieCredits
+                     from crew in movie.Crew
+                     where crew.Name.Equals(nameUniversal)
+                     select crew.Department)
+                let topDepartment = departments
+                    .GroupBy(d => d)
+                    .OrderByDescending(g => g.Count())
+                    .Select(g => g.Key)
+                select new { Name = nameUniversal, TopDepartment = topDepartment };
+            return universalsTopDepartments.Select(u => $"{u.Name} - {u.TopDepartment}");
+        }
+
+        public IEnumerable<string> getMartinAndNolan()
+        {
+            var MartinCrewCast = (
+                 from movie in movieCredits
+                 where movie.Crew.Any(c => c.Job.Equals("Director")
+                                  && c.Name.Equals("Martin Scorsese"))
+                 from cast in movie.Cast
+                 select cast.Name).ToList()
+                .Concat(
+                 from movie in movieCredits
+                 where movie.Crew.Any(c => c.Job.Equals("Director")
+                                          && c.Name.Equals("Martin Scorsese"))
+                 from crew in movie.Crew
+                 select crew.Name)
+                .Distinct()
+                .ToList();
+
+            var NolanCrewCast = (
+                from movie in movieCredits
+                where movie.Crew.Any(c => c.Job.Equals("Director")
+                                 && c.Name.Equals("Christopher Nolan"))
+                from cast in movie.Cast
+                select cast.Name).ToList()
+                .Concat(
+                 from movie in movieCredits
+                 where movie.Crew.Any(c => c.Job.Equals("Director")
+                                          && c.Name.Equals("Christopher Nolan"))
+                 from crew in movie.Crew
+                 select crew.Name)
+                .Distinct()
+                .ToList();
+
+            var intersection = MartinCrewCast.Intersect(NolanCrewCast).ToList();
+
+            return intersection;
+        }
+
+        public IEnumerable<string> getHiddenImpact()
+        {
+            var range =
+                from tmp in
+                    (from movie in movieCredits
+                     from crew in movie.Crew
+                     select new
+                     {
+                         Department = crew.Department,
+                         CastCount = movie.Cast.Count 
+                     })
+                group tmp by tmp.Department into deptGroup
+                select new
+                { 
+                    Department = deptGroup.Key,
+                    AverageCastSize = deptGroup.Average(x => x.CastCount)
+                } 
+                into result
+                orderby result.AverageCastSize descending
+                select result;
+            return range.Select(m => $"{m.Department} - AverageCast: {m.AverageCastSize}");
+        }
+
+        public IEnumerable<string> getJohnnyArchetypes()
+        {
+            var JohnnyArchetypes =
+                from movie in movieCredits
+                from cast in movie.Cast
+                where cast.Name.Equals("Johnny Depp")
+                let firstWord = cast.Character
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .First()
+                group cast by firstWord into g
+                orderby g.Count() descending
+                select new
+                {
+                    Archetype = g.Key,
+                    Count = g.Count()
+                };
+
+            return JohnnyArchetypes.Select(m => $"{m.Archetype} - {m.Count}");
+        }
+    }
+}
